46ea2589dc8888cfbda4813b29a3dc273ac74737
LucasTesche1 <lucas@yujilive.com.br>
2024-08-21 15:06:39 -0300

    add

---
+++ clientes/templates/static/clientes/js/clientes.js
@@ -0,0 +1,23 @@
+function add_carro(){
+    container = document.getElementById("form-carro")
+
+    html = "<br> <div class='row'> <div class='col-md'> <input type='text' placeholder='carro' class='form-control' name='carro' > </div> <div class='col-md'><input type='text' placeholder='Placa' class='form-control' name='placa' ></div> <div class='col-md'> <input type='number' placeholder='ano' class='form-control' name='ano'> </div> </div>"
+
+    container.innerHTML += html
+}
+
+function exibir_form(tipo){
+
+    add_cliente = document.getElementById('adicionar-cliente')
+    att_cliente = document.getElementById('att_cliente')
+
+    if(tipo == "1"){
+        att_cliente.style.display = "none"
+        add_cliente.style.display = "block"
+
+    }else{
+        add_cliente.style.display = "none"
+        att_cliente.style.display = "block"
+    }
+
+}
