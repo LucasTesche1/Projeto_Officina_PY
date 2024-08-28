6ecc8d1479411af4992d0fe5dcbcef4583bd4fe9
LucasTesche1 <lucas@yujilive.com.br>
2024-08-28 13:50:41 -0300

    everthing fixed

--- clientes/templates/static/general/js/base.js
+++ clientes/templates/static/general/js/base.js
@@ -2,28 +2,20 @@ let sidebar = document.querySelector(".sidebar");
 let closeBtn = document.querySelector("#btn");
 let searchBtn = document.querySelector(".bx-search");
 
-closeBtn.addEventListener("click", () =>{
-
+closeBtn.addEventListener("click", ()=>{
     sidebar.classList.toggle("open");
     menuBtnChange();
+});
 
-})
-
-
-searchBtn.addEventListener("click", () =>{
-
+searchBtn.addEventListener("click", ()=>{
     sidebar.classList.toggle("open");
     menuBtnChange();
+});
 
-})
-
-function menuBtnChange(){
-
-    if (sidebar.classList.contains("open")){
-        closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
-
-    }else{
-        closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
-    }
-
+function menuBtnChange() {
+if(sidebar.classList.contains("open")){
+    closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+}else {
+    closeBtn.classList.replace("bx-menu-alt-right","bx-menu");
+}
 }
