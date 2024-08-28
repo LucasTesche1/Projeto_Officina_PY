525b4a1bf0493f77f10a2db57a2596c28f597c74
LucasTesche1 <lucas@yujilive.com.br>
2024-08-28 15:19:41 -0300

    done the first part of the tutorial

--- clientes/templates/static/clientes/js/clientes.js
+++ clientes/templates/static/clientes/js/clientes.js
@@ -2,7 +2,7 @@ function add_carro(){
 
     container = document.getElementById('form-carro');
 
-    html = "<br> <div class = 'row'> <div class = 'col-md'> <input type = 'text' placeholder = 'carro' class = 'form-control' name = 'carro'>  </div> <div class= 'col-md'>  <input type = 'text' placeholder = 'Placa' class = 'form-control' name = 'placa'> </div> <div class= 'col-md'> <input type = 'number' placeholder = 'ano' class = 'form-control' name = 'ano'> </div>  </div>";
+    html = "<br> <div class = 'row'> <div class = 'col-md'> <input type = 'text' placeholder = 'carro' class = 'form-control' name = 'carro'>  </div> <div class= 'col-md'>  <input type = 'text' placeholder = 'placa' class = 'form-control' name = 'placa'> </div> <div class= 'col-md'> <input type = 'number' placeholder = 'ano' class = 'form-control' name = 'ano'> </div>  </div>";
 
     container.innerHTML += html;
 
--- clientes/views.py
+++ clientes/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Cliente,Carro
 from django.http import HttpResponse
+import re
+
 
 def clientes (request):
     if request.method == "GET":
@@ -9,12 +12,35 @@ def clientes (request):
         sobrenome = request.POST.get('sobrenome')
         email = request.POST.get('email')
         cpf = request.POST.get('cpf')
+
         carros = request.POST.getlist('carro')
-        placas = request.POST.getlist('placas')
+        placas = request.POST.getlist('placa')
         anos = request.POST.getlist('ano')
 
-        print(carros)
-        print(placas)
-        print(anos)
+        cliente = Cliente.objects.filter(cpf = cpf)
+        if cliente.exists():
+            #TODO-ADICIONAR MENSAGENS
+            return render(request, 'clientes.html', {'nome':nome, 'sobrenome':sobrenome, 'email':email, 'carros':zip(carros,placas,anos)})
+        if not re.fullmatch(re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+'), email):
+            #TODO-ADICIONAR MENSAGENS
+            return render(request, 'clientes.html', {'nome':nome, 'sobrenome':sobrenome,'cpf':cpf, 'zip': zip(carros,placas,anos)})
+
+        cliente = Cliente(
+            nome = nome,
+            sobrenome = sobrenome,
+            email = email,
+            cpf = cpf,
+        )
+
+        cliente.save()
+
+        for carro, placa, ano in zip(carros, placas, anos):
+            car = Carro(
+                carro = carro,
+                placa = placa,
+                ano = ano,
+                cliente = cliente,
+            )
+            car.save()
 
         return HttpResponse('teste')
