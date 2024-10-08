c5b03d6ae5c01247b93da757a7e0272b2dc04479
LucasTesche1 <lucas@yujilive.com.br>
2024-10-08 14:56:59 -0300

    Done

--- servicos/urls.py
+++ servicos/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('novo_servico/', views.novo_servico, name='novo_servico'),
     path('listar_servico/', views.listar_servico, name='listar_servico'),
     path('servico/<str:identificador>',views.servico, name='servico'),
+    path('gerar_os/<str:identificador>', views.gerar_os, name='gerar_os'),
 ]
--- servicos/views.py
+++ servicos/views.py
@@ -1,9 +1,11 @@
 from contextlib import nullcontext
-
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
 from django.shortcuts import render,get_object_or_404
 from .models import Servico
 from .forms import FormServico
+from fpdf import FPDF
+from io import BytesIO
+
 
 # Create your views here.
 def novo_servico(request):
@@ -32,3 +34,40 @@ def servico(request, identificador):
     servico = get_object_or_404(Servico, identificador=identificador)
 
     return render (request, 'servico.html', {'servico': servico})
+
+def gerar_os(request, identificador):
+    servico = get_object_or_404(Servico, identificador=identificador)
+    pdf = FPDF()
+    pdf.add_page()
+
+    pdf.set_font('Arial', 'B', size=11)
+
+    pdf.set_fill_color(240,240,240)
+    pdf.cell(35,10,'Cliente:', 1, 0, 'L', True)
+    pdf.cell(0,10,f'{servico.cliente.nome}', 1, 1, 'L', True)
+
+    pdf.cell(35,10,'Manutenções:', 1, 0, 'L', True)
+
+    categorias_manutencao = servico.categoria_manutencao.all()
+    for i, manutencao in enumerate(categorias_manutencao):
+
+        pdf.cell(0,10,f'- {manutencao.get_titulo_display()}', 1, 1, 'L', True)
+        if not i == len(categorias_manutencao) -1:
+            pdf.cell(35,10,'',0,0)
+
+    pdf.cell(35,10,'Valor total:', 1, 0, 'L', True)
+    pdf.cell(35,10,f'{servico.preco_total()}',1,1,'L',False)
+
+    pdf.cell(35,10,'Data de início:', 1, 0, 'L', True)
+    pdf.cell(0,10,f'{servico.data_inicio}',1,1,'L',True)
+    pdf.cell(35,10,'Data de entrega:', 1, 0, 'L', True)
+    pdf.cell(0,10,f'{servico.data_entrega}',1,1,'L',True)
+
+    pdf.cell(35,10,'Protocolo:', 1, 0, 'L', True)
+    pdf.cell(35,10,f'{servico.protocolo}',0,1,'L',False)
+
+    pdf_content = pdf.output(dest='S').encode('latin')
+
+    pdf_bytes = BytesIO(pdf_content)
+
+    return FileResponse(pdf_bytes, filename=f'os-{servico.protocolo}.pdf')
